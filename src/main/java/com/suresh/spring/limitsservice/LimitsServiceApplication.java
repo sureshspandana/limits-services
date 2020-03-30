c5c34a021156db711fe5f5c3fdd2fa03f86f960a
sureshspandana <sureshc.behera@gmail.com>
2020-03-30 22:28:36 +0530

    add

--- src/main/java/com/suresh/spring/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/suresh/spring/limitsservice/LimitsServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("maven projct and jenkins");
 		SpringApplication.run(LimitsServiceApplication.class, args);
 	}
 
